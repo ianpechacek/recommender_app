2dd8af0cad21ea4e0ac026fa0c369ac80439cd24
ianpechacek <ian.pechacek@gmail.com>
2025-01-14 12:32:03 +0100

    Update recommender_app.py

--- recommender_app.py
+++ recommender_app.py
@@ -47,15 +47,6 @@ def download_dataset_from_kaggle(dataset_name, zip_path):
         st.info("Dataset already exists. Skipping download.")
         
 
-    # if 'zip_file' not in st.session_state:
-    #     # Open and read the zip file into memory
-    #     with open(zip_path, 'rb') as f:
-    #         file_content = f.read()  # Read the content of the zip file into memory
-        
-    #     # Store the file content in session state
-    #     st.session_state.zip_file = file_content
-
-
 def load_data(path_rating, paths_books):
     # print('Data CWD location: ', path_rating, paths_books)
     ratings = pd.read_csv(path_rating, low_memory=False)
@@ -69,17 +60,6 @@ def null_values_drop(df):
         print(f'Column {column} has {nulls} null values')
     df.dropna(axis=1, how='any', inplace=True, subset=None)
 
-# @st.cache_data
-def search_similar_names(df, record_column, user_column, target_name, threshold=90):
-    matches = []
-    user_ids = [] 
-    for index, record in df[record_column].items():
-        # Skip non-string (e.g., NaN, None, numbers)
-        if isinstance(record, str) and fuzz.partial_ratio(record.lower(), target_name.lower()) >= threshold:
-            matches.append(record)
-            user_ids.append(df[user_column].iloc[index])  # Get corresponding User-ID for the match
-    return np.unique(np.array(matches)), np.unique(np.array(user_ids))
-
 def search_similar_names(df, record_column, user_column, target_name, threshold=80):
     results = process.extract(
         target_name,
@@ -88,12 +68,16 @@ def search_similar_names(df, record_column, user_column, target_name, threshold=
         score_cutoff=threshold
     )
     matches = [match[0] for match in results]
-    user_ids = [
-        df[df[record_column] == match[0]][user_column].values[0]
-        for match in results
-    ]
-    return np.unique(matches), np.unique(user_ids)
+    # user_ids = [
+    #     df[df[record_column] == match[0]][user_column].values[0]
+    #     for match in results
+    # ]
+    return np.unique(matches)
+        #   np.unique(user_ids)
 
+def filter_users(df, record_column, user_column, target_name):
+    users_id = df[df[record_column]==target_name][user_column].unique()
+    return users_id
 
 def matrix_transform(dataset, users, ratings_count_threshold=8):
     books_to_compare = dataset[dataset['User-ID'].isin(users)] \
@@ -159,22 +143,26 @@ def load_data_callback():
                 st.session_state.target_name=st.session_state.text_input
             # st.session_state.dataset = load_data(path_rating, paths_books)
             null_values_drop(st.session_state.dataset)
-            st.session_state.matches, st.session_state.users = search_similar_names(st.session_state.dataset, record_column, user_column, st.session_state.target_name)
+            st.session_state.matches = search_similar_names(st.session_state.dataset, record_column, user_column, st.session_state.target_name)
         st.success("Done!")
 
 def run_model_callback():
     with st.spinner('Calculating recommendations...'):
         st.session_state.selected_book=st.session_state.select_book
+        st.session_state.users = filter_users(st.session_state.dataset, record_column, user_column, st.session_state.selected_book)
         st.session_state.ratings_matrix = matrix_transform(st.session_state.dataset, st.session_state.users )
+        if len(st.session_state.ratings_matrix) > 1:
+            # build cosine similarity matrix
+            cosine_similarity_matrix(st.session_state.ratings_matrix)
 
-        # build cosine similarity matrix
-        # cosine_similarity_matrix(st.session_state.ratings_matrix)
+            # build correlation matrix
+            correlation_matrix(st.session_state.ratings_matrix)
 
-        # # build correlation matrix
-        # correlation_matrix(st.session_state.ratings_matrix)
-
-        # # build knn df
-        # knn_matrix(st.session_state.ratings_matrix, st.session_state.selected_book)
+            # build knn df
+            knn_matrix(st.session_state.ratings_matrix, st.session_state.selected_book)
+        else:
+            del st.session_state.ratings_matrix
+            st.write('Not enough data to build a recommender model...Try a different edition or other book!')
 
 # ==============================================================
 # STREAMLIT
@@ -201,74 +189,72 @@ with st.sidebar:
     if 'target_name' in st.session_state:
         selected_book = st.selectbox('Select the book: ', options=st.session_state.matches, on_change=run_model_callback, key='select_book')
                                  
-# if 'selected_book' in st.session_state:
-#     with st.sidebar:
-#         st.write('Reload page for new search') 
+if 'ratings_matrix' in st.session_state:
+    with st.sidebar:
+        st.write('Reload page for new search') 
 
-#     col1, col2 = st.columns(2, gap='medium')
-#     with col1:
-#         st.header('You might enjoy these books: ')
-#         st.bar_chart(st.session_state.cosine_sim_df[selected_book]\
-#         [st.session_state.cosine_sim_df[selected_book] < 1]
-#         .sort_values(ascending=False)
-#         .head(5)
-#         ,horizontal=True, height=400) 
+    col1, col2 = st.columns(2, gap='medium')
+    with col1:
+        st.header('You might enjoy these books: ')
+        st.bar_chart(st.session_state.cosine_sim_df[selected_book]\
+        [st.session_state.cosine_sim_df[selected_book] < 1]
+        .sort_values(ascending=False)
+        .head(5)
+        ,horizontal=True, height=400) 
   
-#     with col2:
-#         st.header('You might dislike these books: ')
-#         st.bar_chart(st.session_state.cosine_sim_df[selected_book]\
-#         [st.session_state.cosine_sim_df[selected_book] > 0]
-#         .sort_values(ascending=False)
-#         .tail(5)
-#         ,horizontal=True, height=400, color=[255,0,0])
+    with col2:
+        st.header('You might dislike these books: ')
+        st.bar_chart(st.session_state.cosine_sim_df[selected_book]\
+        [st.session_state.cosine_sim_df[selected_book] > 0]
+        .sort_values(ascending=False)
+        .tail(5)
+        ,horizontal=True, height=400, color=[255,0,0])
 
-#     st.markdown(
-#     '<p style="text-align: center; color: grey; font-size: 20px;">Cosine similarity model</p>',
-#     unsafe_allow_html=True
-#     )
+    st.markdown(
+    '<p style="text-align: center; color: grey; font-size: 20px;">Cosine similarity model</p>',
+    unsafe_allow_html=True
+    )
     
 
-#     col1, col2 = st.columns(2, gap='medium')
-#     with col1:
-#         st.header('You might enjoy these books: ')
-#         st.bar_chart(st.session_state.corr_df[selected_book]\
-#         [st.session_state.corr_df[selected_book] < 1]
-#         .sort_values(ascending=False)
-#         .head(5)
-#         ,horizontal=True, height=400) 
+    col1, col2 = st.columns(2, gap='medium')
+    with col1:
+        st.header('You might enjoy these books: ')
+        st.bar_chart(st.session_state.corr_df[selected_book]\
+        [st.session_state.corr_df[selected_book] < 1]
+        .sort_values(ascending=False)
+        .head(5)
+        ,horizontal=True, height=400) 
   
-#     with col2:
-#         st.header('You might dislike these books: ')
-#         st.bar_chart(st.session_state.corr_df[selected_book]\
-#         [st.session_state.corr_df[selected_book] > -0.8]
-#         .sort_values(ascending=False)
-#         .tail(5)
-#         ,horizontal=True, height=400, color=[255,0,0])
+    with col2:
+        st.header('You might dislike these books: ')
+        st.bar_chart(st.session_state.corr_df[selected_book]\
+        [st.session_state.corr_df[selected_book] > -0.8]
+        .sort_values(ascending=False)
+        .tail(5)
+        ,horizontal=True, height=400, color=[255,0,0])
 
-#     st.markdown(
-#     '<p style="text-align: center; color: grey; font-size: 20px;">Correlation model</p>',
-#     unsafe_allow_html=True
-#     )
+    st.markdown(
+    '<p style="text-align: center; color: grey; font-size: 20px;">Correlation model</p>',
+    unsafe_allow_html=True
+    )
     
-#     col1, col2 = st.columns(2, gap='medium')
-#     with col1:
-#         st.header('You might enjoy these books: ')
-#         st.bar_chart(
-#         data = st.session_state.knn_df[1:6],
-#         x = 'title',
-#         y = 'distance',
-#         # .head(5)
-#         horizontal=True, height=400) 
+    col1, col2 = st.columns(2, gap='medium')
+    with col1:
+        st.header('You might enjoy these books: ')
+        st.bar_chart(
+        data = st.session_state.knn_df[1:6],
+        x = 'title',
+        y = 'distance',
+        # .head(5)
+        horizontal=True, height=400) 
   
 
-#     st.markdown(
-#     '<p style="text-align: center; color: grey; font-size: 20px;">KNN model</p>',
-#     unsafe_allow_html=True
-#     )
+    st.markdown(
+    '<p style="text-align: center; color: grey; font-size: 20px;">KNN model</p>',
+    unsafe_allow_html=True
+    )
 # ==============================================================
 
-
-
-st.session_state
+# st.session_state
 
 
